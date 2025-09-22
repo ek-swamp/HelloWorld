7b5d26b79ded0520658302e0d1a566efebb16fa1
Michael Carlsson <CheMical@users.noreply.github.com>
2025-09-22 10:01:28 +0200

    Corrected classname

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello World!"); } }
+public class HelloWorld { public static void main(String[] args) { System.out.println("Hello World!"); } }
